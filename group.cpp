aa4fe5a44c5950486f1c7ac96700d1443d7049b6
Bezawity <103578654+Bezawity@users.noreply.github.com>
2022-05-12 01:08:45 +0300

    Update group.cpp
    Bezawit committed

--- group.cpp
+++ group.cpp
@@ -7,6 +7,7 @@ int main(){
     cout<<"dereje getachew.......................1201438";
     cout<<"Hana Belay............................1207363";
     Cout<<"Eyerusalem Taeme......................1201615";
+    cout<<"Bezawit Yimer..........................1201210";
 return 0;
 }
 
