a1f749fbb6c2a5b1f08f37897fd3009e973013f0
Bezawity <103578654+Bezawity@users.noreply.github.com>
2022-05-12 00:52:11 +0300

    Bezawity

--- group.cpp
+++ group.cpp
@@ -5,6 +5,7 @@ int main(){
     cout<<"abeselom solomon......................1200738";
     cout<<"Eyosiyas Aligas......................1201635";
     cout<<"dereje getachew.......................1201438";
+    cout<<"bezawit yimer.........................1201210";
     cout<<"Hana Belay............................1207363";
     Cout<<"Eyerusalem Taeme......................1201615";
 return 0;
