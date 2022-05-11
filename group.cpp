d6e5a112c6494d6d121b891499f128c52fffa59c
Bezawity <103578654+Bezawity@users.noreply.github.com>
2022-05-12 01:04:40 +0300

    Update group.cpp
    Bezawit committed

--- group.cpp
+++ group.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main(){
 
     cout<<"abeselom solomon......................1200738";
-    cout<<"Eyosiyas Aligas......................1201635";
+    cout<<"Eyosiyas Aligas.......................1201635";
     cout<<"dereje getachew.......................1201438";
     cout<<"Hana Belay............................1207363";
-    Cout<<"Eyerusalem Taeme......................1201615";
+    cout<<"Eyerusalem Taeme......................1201615";
+    cout<<"Bezawit Ymer..........................1201210";
 return 0;
 }
 
