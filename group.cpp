845475f1f65110bb44c4d54588192e8daf1a45fe
Bezawity <103578654+Bezawity@users.noreply.github.com>
2022-05-12 00:39:14 +0300

    Group7.cpp
    A project by group7

--- group.cpp
+++ group.cpp
@@ -5,8 +5,7 @@ int main(){
     cout<<"abeselom solomon......................1200738";
     cout<<"Eyosiyas Aligas......................1201635";
     cout<<"dereje getachew.......................1201438";
-    cout<<"Hana Belay............................1207363";
+    cout<<"Bezawit Yimer.........................1201210";
     return 0;
 }
-
 
